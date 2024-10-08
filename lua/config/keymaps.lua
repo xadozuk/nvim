4b9ff4ca28ed9297680e59dc9dc4d3da0adf9e87
Loïc Demoulin <loic.demoulin@avanade.com>
2024-10-08 14:42:48 +0200

    Import neovim config into git

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,20 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+  -- vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+  -- vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+  -- vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+  -- vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+  -- vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
