971bdec90ad29ecab7469a19bdf227b152ef7f6f
Loïc Demoulin <loic.demoulin@avanade.com>
2024-10-15 11:56:39 +0200

    Update nvim config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,14 @@
 return {
   { "catppuccin/nvim", name = "catppuccin" },
   { "ellisonleao/gruvbox.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
 }
---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      -- filesystem = {
+      --   bind_to_cwd = true,
+      --   cwd_target = {
+      --     sidebar = "tab",
+      --     current = "window",
+      --   },
+      -- },
+    },
+  },
+}
