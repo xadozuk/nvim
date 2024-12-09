b12ade6c7dd09c16562b9567b9a053b53d279d63
Xadozuk <xadozuk@gmail.com>
2024-12-09 13:36:33 +0100

    Update NVim config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,13 +2,13 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
-      -- filesystem = {
-      --   bind_to_cwd = true,
-      --   cwd_target = {
-      --     sidebar = "tab",
-      --     current = "window",
-      --   },
-      -- },
+      filesystem = {
+        -- bind_to_cwd = true,
+        cwd_target = {
+          -- sidebar = "tab",
+          --     current = "window",
+        },
+      },
     },
   },
 }
