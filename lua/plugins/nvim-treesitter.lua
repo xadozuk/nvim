2a88430bbecb5a68bba1be70d7a69c302baa99ff
Xadozuk <xadozuk@gmail.com>
2025-08-16 11:06:34 +0200

    fix: disable php blade support

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { "blade", "php_only" })
+      -- vim.list_extend(opts.ensure_installed, { "blade", "php_only" })
       opts.endwise = { enable = true }
     end,
     config = function(_, opts)
@@ -17,14 +17,14 @@ return {
       ---@class ParserInfo[]
       local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 
-      parser_config.blade = {
-        install_info = {
-          url = "https://github.com/EmranMR/tree-sitter-blade",
-          files = { "src/parser.c" },
-          branch = "main",
-        },
-        filetype = "blade",
-      }
+      -- parser_config.blade = {
+      --   install_info = {
+      --     url = "https://github.com/EmranMR/tree-sitter-blade",
+      --     files = { "src/parser.c" },
+      --     branch = "main",
+      --   },
+      --   filetype = "blade",
+      -- }
     end,
   },
 }
