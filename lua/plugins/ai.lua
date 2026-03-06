87d301cf07145cddf1cc7adb8a4dd4389fd9ec40
Xadozuk <xadozuk@gmail.com>
2026-03-06 18:42:27 +0100

    feat: add yazi and opencode plugins

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -24,30 +24,30 @@ return {
       })
     end,
   },
-  {
-    "yetone/avante.nvim",
-    opts = {
-      provider = "copilot",
-      providers = {
-        copilot = {
-          model = "claude-sonnet-4",
-        },
-      },
-      windows = {
-        input = {
-          height = 16,
-        },
-      },
-      system_prompt = function()
-        local hub = require("mcphub").get_hub_instance()
-        return hub and hub:get_active_servers_prompt() or ""
-      end,
-      -- Using function prevents requiring mcphub before it's loaded
-      custom_tools = function()
-        return {
-          require("mcphub.extensions.avante").mcp_tool(),
-        }
-      end,
-    },
-  },
+  -- {
+  --   "yetone/avante.nvim",
+  --   opts = {
+  --     provider = "copilot",
+  --     providers = {
+  --       copilot = {
+  --         model = "claude-sonnet-4.6",
+  --       },
+  --     },
+  --     windows = {
+  --       input = {
+  --         height = 16,
+  --       },
+  --     },
+  --     system_prompt = function()
+  --       local hub = require("mcphub").get_hub_instance()
+  --       return hub and hub:get_active_servers_prompt() or ""
+  --     end,
+  --     -- Using function prevents requiring mcphub before it's loaded
+  --     custom_tools = function()
+  --       return {
+  --         require("mcphub.extensions.avante").mcp_tool(),
+  --       }
+  --     end,
+  --   },
+  -- },
 }
